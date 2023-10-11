13ae2d5f7de9d40bde32fd94fc046911e5e56571
hitesh09saini <hitesh9694saini@gmail.com>
2023-10-11 19:07:37 +0530

    f kg

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -3,8 +3,13 @@ import './body.css';
 const Body = () => {
     return (
         <div className='body-cont'>
+            <div className="plus"><i class="fa-solid fa-address-card fa-2xl" style={{color: "#2a092a"}}></i></div>
             <div className='input'>
                 <input type="search" id="search" name="search" placeholder='Search College' />
+                <div className='icons'>
+                    <i class="fa-solid fa-magnifying-glass fa-xl" style={{ color: "white" }}></i>
+                </div>
+
             </div>
 
         </div>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from'../../assets/CollegeIDLOGO.png'
+import logo from'../../assets/collegeID.png'
 import './header.css'
 
 const Header = () => {
