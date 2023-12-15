65405cc4723d97126e26beef3afc86b640c400d3
Hitesh Saini <hitesh9694saini@gmail.com>
2023-12-15 18:15:30 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 
 import './App.css';
 import Header from './Components/Header/Header';
-// import Body from './Components/Body/Body';
-// import Footer from './Components/Footer/Footer';
-import RegistrationForm from './Components/RegistrationForm'
+ import Body from './Components/Body/Body';
+ import Footer from './Components/Footer/Footer';
+
 function App() {
 
   return (
     <div className=" ">
-      {/* // <Header/>
-      // <Body />
-      // <Footer/> */}
-        <RegistrationForm/>
-
+      <Header/>
+       <Body />
+       <Footer/>
     </div>
   );
 }
