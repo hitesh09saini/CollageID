98729dd09dcc5ef52a269d22a9b42d500a882b14
Hitesh Saini <hitesh9694saini@gmail.com>
2024-01-01 23:10:19 +0530

    p

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import Header from './Components/Header/Header';
 function App() {
 
   return (
-    <div className=" ">
+    <div >
       <Header/>
-       <Body />
+       <Body/>
        <Footer/>
     </div>
   );
